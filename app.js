81ba5d2e659edaf3e4520d69aa06027158d36a1e
Raymond <raymondjohnson@pursuit.org>
2023-07-09 14:41:23 -0400

    Initial commit

---
+++ app.js
@@ -0,0 +1,27 @@
+// DEPENDENCIES
+const express = require("express");
+const cors = require("cors");
+//
+
+// CONFIGURATION
+const app = express();
+
+// MIDDLEWARE
+app.use(express.json()); // Parse incoming JSON
+app.use(cors());
+
+// ROUTES
+app.get("/", (req, res) => {
+  res.send("Welcome to Captains Log App");
+});
+
+//const logsController = require("./controllers/logsController.js");
+//app.use("/logs", logsController);
+
+// 404 PAGE
+app.get("*", (req, res) => {
+  res.json({ error: "Page not found" });
+});
+
+// EXPORT
+module.exports = app;
